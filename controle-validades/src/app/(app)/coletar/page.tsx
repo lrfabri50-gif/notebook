169b4f4a55c7449c56b54cab7fac3ea38cff93a4
Luiz Renato Fabricio <lrfabri50@gmail.com>
2026-09-10 19:29:21 -0300

    fix: Desabilitar leitor nativo experimental que pode falhar no iOS Safari

--- controle-validades/src/app/(app)/coletar/page.tsx
+++ controle-validades/src/app/(app)/coletar/page.tsx
@@ -53,14 +53,12 @@ export default function ColetaPage() {
     if (isScanning) {
       html5QrCode = new Html5Qrcode("reader", {
         verbose: false,
-        useBarCodeDetectorIfSupported: true,
         formatsToSupport: [
           Html5QrcodeSupportedFormats.EAN_13,
           Html5QrcodeSupportedFormats.EAN_8,
           Html5QrcodeSupportedFormats.CODE_128,
           Html5QrcodeSupportedFormats.UPC_A,
-          Html5QrcodeSupportedFormats.UPC_E,
-          Html5QrcodeSupportedFormats.CODE_39
+          Html5QrcodeSupportedFormats.UPC_E
         ]
       });
       html5QrCode.start(
